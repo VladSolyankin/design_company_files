4cba75659ceab15951c287a0faca79bb01b7753f
VladSolyankin <darnell.dewberry@mail.ru>
2023-03-15 13:47:23 +0300

    commit all files

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", () => {
+	let viewMoreButton = document.querySelector('.view_button')
+	viewMoreButton.addEventListener('click', () => {
+		window.location.href = "portfolio.html"
+		console.log("123")
+	})
+})
