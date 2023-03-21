eabb3ed0dc14200880f19de144b56dad756849b9
VladSolyankin <darnell.dewberry@mail.ru>
2023-03-21 18:48:23 +0300

    commit all files

--- src/js/mail.js
+++ src/js/mail.js
@@ -1,10 +1,7 @@
-document.querySelector('html').addEventListener('DOMContentLoaded', () => {
-	let sendButton = document.getElementById("send_button")
-	let message = document.querySelector('textarea')
-	let mailForm = document.querySelector('.main__form-mail')
+let sendButton = document.querySelector(".info__button-submit")
+let message = document.querySelector('textarea')
 
-	sendButton.addEventListener('click', (e) => {
-		e.preventDefault()
-		mailForm.action = `mailto:ourteam@example.com?body=${message}`
-	})
+sendButton.addEventListener('click', (e) => {
+	document.querySelector('.main__content').action =
+		`mailto:ourmail@example.com?body=${message.textContent}`
 })
